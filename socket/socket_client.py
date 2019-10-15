8769a35420cb15784a0c99e073ba5965f17c3308
The Software Guy <41910161+softwareguycoder@users.noreply.github.com>
2019-10-15 13:08:45 -0400

    Update socket_client.py
    Add error handling
    Close socket in finally block
    Make more verbose

--- socket/socket_client.py
+++ socket/socket_client.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import socket
 
 def main():
@@ -15,13 +17,17 @@ def main():
     print("Trying to connect to the server at 127.0.0.1 on port 50000...")
     try:
         sock.connect(remote_address)
-        socket.sendall('Hello world!\n")
+        print("C: <connected to server 127.0.0.1 on port 5000>")
+        print("C: Hello, world!")
+        socket.sendall('Hello world!\n')
+        # listen for the server's response, up to 100 bytes long
+        reply = sock.recv(100)
+        print("S: {}".format(reply))
     except:
         print("ERROR: Socket operation failed.")
-
-    # listen for the server's response, up to 100 bytes long
-    sock.recv(100)
-    
+    finally:
+        sock.close()
+        
     
 if __name__=="__main__":
     main()
