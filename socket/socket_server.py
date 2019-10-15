02572c19ef2b0e91efa98af1d1e00706fe9d193e
The Software Guy <41910161+softwareguycoder@users.noreply.github.com>
2019-10-15 13:33:34 -0400

    Update socket_server.py
    Working TCP server

--- socket/socket_server.py
+++ socket/socket_server.py
@@ -9,11 +9,17 @@ def main():
     # listen on
     our_address = ('127.0.0.1', 50000)
     
+    print("S: <binding to 127.0.0.1, port 50000>")
+    
     # set up the socket to be bound to our_address
     sock.bind(our_address)
-    
+         
     # listen on the address and port for new incoming connections
     sock.listen(1)
+    
+    print("S: <listening mode activated>")
+
+    print("S: <accepting new connections>")
     
     # wait until a client connects, and then communicate with it
     sock_to_client, remote_address = sock.accept()
@@ -21,27 +27,26 @@ def main():
     try:
         content = ""        # data received
         while(True):
-            content += sock.recv(100)
+            content += sock_to_client.recv(100).decode('utf-8')
             if len(content) == 0:
                 print("C: <disconnected>")
                 break
             if not "\n" in content:
                 continue
-            if not "HELO\n" in content:
-                sock_to_client.sendall("500 Internal server error.\n")
+            if not 'Hello world!\n' in content:
+                sock_to_client.sendall("500 Internal server error.\n".encode('utf-8'))
                 break
-            sock_to_client.sendall("201 OK. The world says hi.\n")
-        
-   except:       
-       print("ERROR: Socket communication error.")
-   finally:   
+            print("C: {}".format(content.replace("\n", "")))
+            sock_to_client.sendall("201 OK. The world says hi.\n".encode('utf-8'))
+            content = ""
+    except Exception as e:       
+       print("ERROR: Socket communication error. {}".format(e))
+    finally:   
        sock_to_client.close()
-       
-    sock.close()
+       sock.close()
+
     pass
     
     
 if __name__=="__main__":
     main()
-       
-        
