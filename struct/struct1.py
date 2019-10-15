cd4cb018d193578d70e2441871a584215848e5ee
The Software Guy <41910161+softwareguycoder@users.noreply.github.com>
2019-10-15 14:27:50 -0400

    Update struct1.py

--- struct/struct1.py
+++ struct/struct1.py
@@ -3,24 +3,24 @@
 # NOTE: This script utilizes Python 2.7!
 
 import binascii
-import re
 import struct
 
-replchars = re.compile(r'[\n\r]')
 
-
-def replchars_to_hex(match):
-    return r'\x{0:02x}'.format(ord(match.group()))
+# This method prints a structured data val as hex values
+# separated by \x in a string, to the console
+def print_structured_data(val):
+    hexstring = binascii.hexlify(val)
+    sx = r"\x" + r"\x".join(hexstring[n : n+2] for n in range(0, len(hexstring), 2))
+    print sx
+    pass
     
-
+    
 # This method packs a list of integers and then prints out the
 # hex values that result after the pack operation    
 def main():
     a = [5,34,21,75,20]
-    val = struct.pack('5B', *a)
-    hexstring = binascii.hexlify(val)
-    sx = r"\x" + r"\x".join(hexstring[n : n+2] for n in range(0, len(hexstring), 2))
-    print sx
+    val = struct.pack('5B', *a)s
+    print_structured_data(val)
     
     
 if __name__=="__main__":
